6559eaf958343714870bdbc60d6d811b0ded9526
Lucia <luciarigoni@live.com>
2022-09-22 10:51:14 -0300

    Initial Commit

---
+++ Dicee-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-Swift
+//
+//  Created by Lucia Rigoni Freire on 22/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee-Swift/Dicee_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftApp.swift
+//  Dicee-Swift
+//
+//  Created by Lucia Rigoni Freire on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
